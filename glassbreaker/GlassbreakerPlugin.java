f1a0abd9d509034667bd4b00d65bd10cd3469e6e
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-04-19 18:31:27 -0400

    Create GlassbreakerPlugin.java

---
+++ glassbreaker/GlassbreakerPlugin.java
@@ -0,0 +1,21 @@
+package com.Fjc.GlassbreakerPlugin.GlassbreakerPlugin
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GlassbreakerPlugin extends JavaPlugin implements Listener {
+	//Startup
+	@Override
+	public void onEnable(){
+		
+		
+	}
+	//Shutdown
+	@Override
+	public void onDisable(){
+		
+		
+	}
+	
+	
+}
