5f68d31415ef2471f599b7c2ea8aa40d524f269a
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-04-20 01:22:42 -0400

    Rewrote main.java, now built on Spigot API instead of Bukkit

--- glassbreaker/main.java
+++ glassbreaker/main.java
@@ -1,3 +1,5 @@
+package com.Fjc.instantglassbreak;
+
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
@@ -9,58 +11,66 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GlassbreakerPlugin extends JavaPlugin implements Listener {
-    
+public class InstantGlassBreak extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
-        // Register events
         getServer().getPluginManager().registerEvents(this, this);
-        
-        // Register the Glassbreaker enchantment
-        Enchantment.registerEnchantment(new GlassbreakerEnchantment());
+        registerEnchantment(new EnchantmentWrapper(101) {
+            @Override
+            public String getName() {
+                return "InstantGlassBreak";
+            }
+
+            @Override
+            public int getMaxLevel() {
+                return 1;
+            }
+
+            @Override
+            public int getStartLevel() {
+                return 1;
+            }
+
+            @Override
+            public EnchantmentTarget getItemTarget() {
+                return EnchantmentTarget.TOOL;
+            }
+
+            @Override
+            public boolean canEnchantItem(ItemStack item) {
+                return item.getType() == Material.NETHERITE_PICKAXE;
+            }
+
+            @Override
+            public boolean conflictsWith(Enchantment other) {
+                return false;
+            }
+
+            @Override
+            public boolean canEnchantItem(Material item) {
+                return item == Material.NETHERITE_PICKAXE;
+            }
+        });
     }
-    
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
-        
-        // Check if the player's held item has the Glassbreaker enchantment
-        if (item.containsEnchantment(Enchantment.getByKey(GlassbreakerEnchantment.KEY))) {
+        if (item.containsEnchantment(Enchantment.getByKey(EnchantmentWrapper.getKey(101)))) {
             if (event.getBlock().getType() == Material.GLASS) {
-                event.setCancelled(true); // Cancel the normal block break event
-                event.getBlock().breakNaturally(item); // Break the glass block instantly
+                event.setCancelled(true);
+                event.getBlock().breakNaturally(item);
             }
         }
     }
-}
 
-class GlassbreakerEnchantment extends EnchantmentWrapper {
-    
-    public static final String KEY = "glassbreaker";
-    
-    public GlassbreakerEnchantment() {
-        super(KEY);
-    }
-    
-    @Override
-    public String getName() {
-        return "Glassbreaker";
-    }
-    
-    @Override
-    public int getMaxLevel() {
-        return 1;
-    }
-    
-    @Override
-    public EnchantmentTarget getItemTarget() {
-        return EnchantmentTarget.TOOL; // Can only be applied to tools
-    }
-    
-    @Override
-    public boolean canEnchantItem(ItemStack item) {
-        // Allow enchantment on tools
-        return item.getType().toString().endsWith("_PICKAXE"); // Allow only pickaxes, add more if needed
+    private void registerEnchantment(Enchantment enchantment) {
+        try {
+            Enchantment.registerEnchantment(enchantment);
+        } catch (IllegalArgumentException ignored) {
+            // Enchantment is already registered, do nothing
+        }
     }
 }
