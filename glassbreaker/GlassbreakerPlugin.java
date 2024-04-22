186eb2ee05c6ac3203ab51c6fde882d977d69ac6
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-04-22 11:55:27 -0400

    Startup and shutdown console log

--- glassbreaker/GlassbreakerPlugin.java
+++ glassbreaker/GlassbreakerPlugin.java
@@ -7,13 +7,15 @@ public class GlassbreakerPlugin extends JavaPlugin implements Listener {
 	//Startup
 	@Override
 	public void onEnable(){
-		
+		this.log = getLogger();
+		this.log.info("Starting plugin!");
 		
 	}
 	//Shutdown
 	@Override
 	public void onDisable(){
-		
+		this.log = this.getLogger();
+		this.log.info("Plugin was successfully disabled")
 		
 	}
 	
