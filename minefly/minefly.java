70925abf718a52d6316a7ba3669f0a05f24fb782
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-04-22 21:51:20 -0400

    Changes speed while flying. Probably doesn't work but someone has to try

---
+++ minefly/minefly.java
@@ -0,0 +1,20 @@
+public class FlyingMiningSpeedPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // This line will register events, probably
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
+        Player player = event.getPlayer();
+        if (event.isFlying()) {
+            // When a player starts flying, these events are called
+            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(20.0); // Is 20 too much?
+        } else {
+            // When a player stops flying, these events are called
+            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(4.0); // Default speed of 4
+        }
+    }
+}
