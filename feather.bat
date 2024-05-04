9644ab150d933b20bd9a277f3bdeee727200c753
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-04 19:57:18 -0400

    Create feather.bat
    Windows only

---
+++ feather.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Potential start script for local hosting
+:: Imperium developers do not run this on a Linux build it will NOT work
+:: Adjust memory as needed
+java -Xmx32G -Xms24G -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -jar paper-1.20.4.jar --nogui
+
+REM Restart
+echo Restart in 5 4 3 2 1 boom
+goto :start
