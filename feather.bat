eeddd82b5c20ee5b2db24aeff50fb3698d30e048
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-05 12:22:38 -0400

    Update feather.bat

--- feather.bat
+++ feather.bat
@@ -2,8 +2,8 @@
 :: Potential start script for local hosting
 :: Imperium developers do not run this on a Linux build it will NOT work
 :: Adjust memory as needed
-java -Xmx32G -Xms24G -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -jar paper-1.20.4.jar --nogui
+java -Xmx40G -Xms32G -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -jar paper-1.20.4.jar --nogui
 
-REM Restart
+@rem Restart
 echo Restart in 5 4 3 2 1 boom
 goto :start
