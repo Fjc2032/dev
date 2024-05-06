9f2a9b0d287f1cfbbcc074a255b438da84a8bfb5
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-06 08:37:27 -0400

    Updated comments and DIR

--- featherboot/endreset.bat
+++ featherboot/endreset.bat
@@ -6,7 +6,8 @@ echo Hello. This script resets the main End island. Probably.
 timeout /t 5 /nobreak
 echo Written by Fjc
 
-timeout /t 3 ::Directories left blank until local directory is available
+timeout /t 3
+@rem Root directories left blank until local directory is available for use (i don't know the file path yet)
 del C:\\\\\world_the_end\DIM1\region\r.0.0.mca
 del C:\\\\\world_the_end\DIM1\region\r.0.1.mca
 del C:\\\\\world_the_end\DIM1\region\r.1.0.mca
@@ -19,7 +20,7 @@ echo Reset complete. Server restarting...
 timeout /t 5 /nobreak
 
 ::Invoke restart
-cd ::someDirectory
+cd C:\\\\someDirectory\server
 start start.bat
 exit
 
