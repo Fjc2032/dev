a032ad76f04354f7d88cec8c48e4d985ba909d08
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-07 07:20:50 -0400

    Update commands.java

--- minefly/commands.java
+++ minefly/commands.java
@@ -6,11 +6,11 @@ public class MineFly extends JavaPlugin implements Listener {
       if (args.length == 0){ //If there are no args, do this:
       sender.sendMessage(ChatColor.RED + "Missing arguments");
       } else {
-        if (args [0].equalsIgnoreCase("reload")){
+        if (args [0].equalsIgnoreCase("reload")){ //If argument "reload" is present, do this:
           reloadConfig();
           sender.sendMessage(ChatColor.GREEN + "MineFly was successfully reloaded");
         }
       }
     }
   }
-}
+} //Did I use too many brackets
