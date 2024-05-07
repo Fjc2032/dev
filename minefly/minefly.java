2ed30433a28299b118445832ff93dcae486b05df
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-06 22:42:09 -0400

    Fixed hashing and class import

--- minefly/minefly.java
+++ minefly/minefly.java
@@ -1,3 +1,9 @@
+import org.bukkit.attribute.Attribute;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class MineFly extends JavaPlugin implements Listener {
 
     @Override
@@ -14,16 +20,17 @@ public class MineFly extends JavaPlugin implements Listener {
             // When a player starts flying, these events are called
             double flyingSpeed = getConfig().getDouble("set-atk-speed.flying", 20.0);
             player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(flyingSpeed); // Is 20 too much?
-            if(getConfig().getString("set-atk-speed") ==null
         } else {
             // When a player stops flying, these events are called
             double notFlyingSpeed = getConfig().getDouble("set-atk-speed.not-flying", 4.0);
             player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(notFlyingSpeed); // Default speed of 4
         }
 
-        if(getConfig().getString("set-atk-speed.flying") ==null {
-           getConfig().getString("set-atk-speed.flying") = 20.0
-            getConfig().getString("set-atk-speed.not-flying") = 4.0
-
+        if (getConfig().getString("set-atk-speed.flying") == null) {
+            getConfig().set("set-atk-speed.flying", 20.0);
+            getConfig().set("set-atk-speed.not-flying", 4.0);
+            saveConfig();
+        }
     }
 }
+
