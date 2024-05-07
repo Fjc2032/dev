376a7fa5288f3191b099ea70f9a5e6a69939666a
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-07 18:25:32 -0400

    Updated script notice

--- featherboot/endreset.bat
+++ featherboot/endreset.bat
@@ -1,7 +1,8 @@
 @echo off
 java -Xmx1G -Xms1G -jar paper-1.20.4.jar --nogui
-
+@rem Low mem cap used since you will be stopping the server immediately after anyways...
 ::When running this script, stop the server immediately after starting to reset the files.
+::The following commands will run after the server is stopped.
 echo Hello. This script resets the main End island. Probably.
 timeout /t 5 /nobreak
 echo Written by Fjc
@@ -19,7 +20,7 @@ del C:\\\\\world_the_end\DIM1\region\r.1.1.mca
 echo Reset complete. Server restarting...
 timeout /t 5 /nobreak
 
-::Invoke restart
+::Invoke restart with main script; uses higher memory cap
 cd C:\\\\someDirectory\server
 start start.bat
 exit
