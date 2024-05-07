318a88ea9fda7adf8199d67f0dedf07c29d06fa5
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-06 22:40:12 -0400

    Fixed config.yml generation

--- minefly/minefly.java
+++ minefly/minefly.java
@@ -12,11 +12,18 @@ public class MineFly extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         if (event.isFlying()) {
             // When a player starts flying, these events are called
-            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(20.0); // Is 20 too much?
+            double flyingSpeed = getConfig().getDouble("set-atk-speed.flying", 20.0);
+            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(flyingSpeed); // Is 20 too much?
             if(getConfig().getString("set-atk-speed") ==null
         } else {
             // When a player stops flying, these events are called
-            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(4.0); // Default speed of 4
+            double notFlyingSpeed = getConfig().getDouble("set-atk-speed.not-flying", 4.0);
+            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(notFlyingSpeed); // Default speed of 4
         }
+
+        if(getConfig().getString("set-atk-speed.flying") ==null {
+           getConfig().getString("set-atk-speed.flying") = 20.0
+            getConfig().getString("set-atk-speed.not-flying") = 4.0
+
     }
 }
