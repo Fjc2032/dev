79ebc69682aa51fccc8bf8c5ad6f1ec4c893b9da
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-05-19 11:35:53 -0400

    Rewrote config initalise

--- minefly/minefly.java
+++ minefly/minefly.java
@@ -6,29 +6,42 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MineFly extends JavaPlugin implements Listener {
 
+    private static final double DEFAULT_FLYING_SPEED = 20.0;
+    private static final double DEFAULT_NOT_FLYING_SPEED = 4.0;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         // This line will register events, probably
         getServer().getPluginManager().registerEvents(this, this);
+        initializeConfig();
     }
 
     @EventHandler
     public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
         Player player = event.getPlayer();
-        if (event.isFlying()) {
-            // When a player starts flying, these events are called
-            double flyingSpeed = getConfig().getDouble("set-atk-speed.flying", 20.0); //Flying speed can now be modified in config.yml
-            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(flyingSpeed); // Is 20 too much?
-        } else {
-            // When a player stops flying, these events are called
-            double notFlyingSpeed = getConfig().getDouble("set-atk-speed.not-flying", 4.0); //Base speed can now be modified in config.yml
-            player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(notFlyingSpeed); // Default speed of 4
-        }
+        ConfigurationSection configSection = getConfig().getConfigurationSection("set-atk-speed");
 
-        if (getConfig().getString("set-atk-speed.flying") == null) {
-            getConfig().set("set-atk-speed.flying", 20.0);
-            getConfig().set("set-atk-speed.not-flying", 4.0);
+        if (configSection != null) {
+            double flyingSpeed = configSection.getDouble("flying", DEFAULT_FLYING_SPEED);
+            double notFlyingSpeed = configSection.getDouble("not-flying", DEFAULT_NOT_FLYING_SPEED);
+
+            if (event.isFlying()) {
+                player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(flyingSpeed);
+            } else {
+                player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(notFlyingSpeed);
+        }
+    }
+}
+
+private void initializeConfig() {
+    ConfigurationSection configSecction = getConfig().getConfiguration("set-atk-speed");
+
+    if (configSection == null) {
+        getConfig().createSection("set-atk-speed");
+        configSection = getConfig().getConfigurationSection("set-atk-speed);
+        configSection.set("flying", DEFAULT_FLYING_SPEED);
+        configSection.set("not-flying", DEFAULT_NOT_FLYING_SPEED);
             saveConfig();
         }
     }
