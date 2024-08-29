856f0fa7bd40de84e07cd48f7c97248554924edf
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-08-28 20:57:30 -0400

    Delete feather.bat

--- feather.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Potential start script for local hosting
-:: Imperium developers do not run this on a Linux build it will NOT work
-:: Adjust memory as needed
-java -Xmx40G -Xms32G -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -jar paper-1.20.4.jar --nogui
-
-@rem Restart
-echo Restart in 5 4 3 2 1 boom
-goto :start
