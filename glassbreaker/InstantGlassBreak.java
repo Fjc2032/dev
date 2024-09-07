e1f19a73c1049e8729a471c194d0f4a56e621460
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-09-07 03:43:02 -0400

    Import all glass types

--- glassbreaker/InstantGlassBreak.java
+++ glassbreaker/InstantGlassBreak.java
@@ -26,7 +26,7 @@ public class InstantGlassBreak extends JavaPlugin implements Listener {
             f.set(null, true);
         } catch (Exception e) {
             e.printStackTrace();
-        
+
         }
 
     }
@@ -53,8 +53,30 @@ public class InstantGlassBreak extends JavaPlugin implements Listener {
                 Material.MAGENTA_STAINED_GLASS_PANE,
                 Material.LIGHT_BLUE_STAINED_GLASS,
                 Material.LIGHT_BLUE_STAINED_GLASS_PANE,
-                Material.
-//I will add the rest of the glass later im so tired lmao
+                Material.YELLOW_STAINED_GLASS,
+                Material.YELLOW_STAINED_GLASS_PANE,
+                Material.LIME_STAINED_GLASS,
+                Material.LIME_STAINED_GLASS_PANE,
+                Material.PINK_STAINED_GLASS,
+                Material.PINK_STAINED_GLASS_PANE,
+                Material.GRAY_STAINED_GLASS,
+                Material.GRAY_STAINED_GLASS_PANE,
+                Material.LIGHT_GRAY_STAINED_GLASS,
+                Material.LIGHT_GRAY_STAINED_GLASS_PANE,
+                Material.CYAN_STAINED_GLASS,
+                Material.CYAN_STAINED_GLASS_PANE,
+                Material.PURPLE_STAINED_GLASS,
+                Material.PURPLE_STAINED_GLASS_PANE,
+                Material.BLUE_STAINED_GLASS,
+                Material.BLUE_STAINED_GLASS_PANE,
+                Material.BROWN_STAINED_GLASS,
+                Material.BROWN_STAINED_GLASS_PANE,
+                Material.GREEN_STAINED_GLASS,
+                Material.GREEN_STAINED_GLASS_PANE,
+                Material.RED_STAINED_GLASS,
+                Material.RED_STAINED_GLASS_PANE,
+                Material.BLACK_STAINED_GLASS,
+                Material.BLACK_STAINED_GLASS_PANE
         };
 
         boolean isGlass = false;
