ec57eb8504540f5fc24568cd1f6e1d62656ab2c9
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-11-16 15:29:51 -0500

    Optimize getCartWIthPlayer() and remove redundant methods

--- MinecartCollider/src/main/java/dev/Fjc/minecartCollider/MinecartCollider.java
+++ MinecartCollider/src/main/java/dev/Fjc/minecartCollider/MinecartCollider.java
@@ -1,7 +1,6 @@
 package dev.Fjc.minecartCollider;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Minecart;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Vehicle;
 import org.bukkit.entity.minecart.RideableMinecart;
 import org.bukkit.event.EventHandler;
@@ -10,13 +9,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
 
 public final class MinecartCollider extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info(ChatColor.GREEN + "Plugin loaded.");
 
     }
 
@@ -25,39 +24,28 @@ public final class MinecartCollider extends JavaPlugin implements Listener {
         // I'll fill this later
     }
 
-    //Returns null if the cart is empty, or if the passenger is not a player
+
     public static RideableMinecart getCartWithPlayer(Vehicle vehicle) {
-        RideableMinecart cart = getRidableMinecart(vehicle);
+
+        //Declare "cart" as a RideableMinecart vehicle
+        RideableMinecart cart = (RideableMinecart) vehicle;
+
+        //If "cart" is not a vehicle, or if the vehicle is empty, return null
         if (cart == null || cart.isEmpty())
             return null;
-        Entity passenger = GetPassenger((Minecart)cart);
-        if (passenger == null || !(passenger instanceof org.bukkit.entity.Player))
-            return null;
-        return cart;
+
+        //If the cart is not empty, and the passenger is a player, return "cart". Otherwise, exit "null"
+        if (!cart.isEmpty() && cart.getPassengers() instanceof org.bukkit.entity.Player)
+            return cart;
+        return null;
     }
 
-    //Returns null if the cart cannot be ridden (maybe it's a chest minecart or smth)
-    public static RideableMinecart getRidableMinecart(Vehicle vehicle) {
-        RideableMinecart cart = null;
-        if (!(vehicle instanceof RideableMinecart))
-            return null;
-        cart = (RideableMinecart)vehicle;
-        return cart;
-    }
 
-    //Lists all entities in minecart, and returns null if the list is empty. This check is kind of redundant.
-    public static Entity GetPassenger(Minecart toCart) {
-        List<Entity> passengers = toCart.getPassengers();
-        if (passengers.isEmpty())
-            return null;
-        return passengers.getFirst();
-    }
-
-    //Cancels collision event if "cart" variable is null.
+    //If "cart" returns not null, cancel any collision event.
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onCollision(VehicleEntityCollisionEvent event) {
         RideableMinecart cart = MinecartCollider.getCartWithPlayer(event.getVehicle());
-        if (cart == null) {
+        if (cart != null) {
             event.setCancelled(true);
             event.setCollisionCancelled(true); //This won't work in the next API version, but for now it's fine
             event.setPickupCancelled(true);
